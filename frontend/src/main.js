9fe44e447bf10f1248a1f1946264dd8409ab019f
Bruno C. Kohn <bcontelk@gmail.com>
2025-06-17 10:25:03 -0300

    update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,21 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './index.css';
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
 
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
