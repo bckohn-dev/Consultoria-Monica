9b29d2d4cbf8e4796a9128b00647b335cd2d25ad
Bruno C. Kohn <bcontelk@gmail.com>
2025-06-17 12:07:12 -0300

    fix api/carrossel

--- api/carrossel.js
+++ api/carrossel.js
@@ -1,35 +1,36 @@
-import { initializeApp, cert, getApps } from "firebase-admin/app";
-import { getFirestore } from "firebase-admin/firestore";
+import { getStorage } from "firebase-admin/storage";
+import { getApps, initializeApp, cert } from "firebase-admin/app";
 
 const {
   FIREBASE_ADMIN_PROJECT_ID,
-  FIREBASE_CLIENT_EMAIL,
-  FIREBASE_PRIVATE_KEY,
+  FIREBASE_ADMIN_CLIENT_EMAIL,
+  FIREBASE_ADMIN_PRIVATE_KEY,
 } = process.env;
-
-if (!FIREBASE_ADMIN_PROJECT_ID || !FIREBASE_CLIENT_EMAIL || !FIREBASE_PRIVATE_KEY) {
-  console.error("🚨 Variáveis de ambiente do Firebase não configuradas!");
-}
 
 if (!getApps().length) {
   initializeApp({
     credential: cert({
       projectId: FIREBASE_ADMIN_PROJECT_ID,
-      clientEmail: FIREBASE_CLIENT_EMAIL,
-      privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      clientEmail: FIREBASE_ADMIN_CLIENT_EMAIL,
+      privateKey: FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, "\n"),
     }),
+    storageBucket: `${FIREBASE_ADMIN_PROJECT_ID}.appspot.com`,
   });
 }
 
-const db = getFirestore();
-
 export default async function handler(req, res) {
   try {
-    const snapshot = await db.collection("carrossel").get();
-    const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-    res.status(200).json(data);
-  } catch (err) {
-    console.error("Erro ao buscar carrossel:", err);
-    res.status(500).json({ erro: "Erro ao buscar carrossel" });
+    const bucket = getStorage().bucket();
+    const [files] = await bucket.getFiles({ prefix: "carrossel/" });
+    const urls = await Promise.all(
+      files.map(file => file.getSignedUrl({
+        action: 'read',
+        expires: '03-01-2500'
+      }).then(urls => urls[0]))
+    );
+    res.status(200).json(urls);
+  } catch (error) {
+    console.error("Erro ao buscar imagens do carrossel:", error);
+    res.status(500).json({ erro: "Erro ao buscar imagens" });
   }
 }
